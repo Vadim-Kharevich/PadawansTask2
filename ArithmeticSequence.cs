f683bb445892b314987bdfc0768210851aa75821
Vadim-Kharevich <41491334+Vadim-Kharevich@users.noreply.github.com>
2019-02-12 10:20:56 +0300

    Update ArithmeticSequence.cs

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -6,12 +6,20 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
+            if (count <= 0)
+            {
+                throw new ArgumentException();
+            }
             int sum = 0;
             int sumadd = 0;
-            for (int i = 0; i < count; i++)
+            for(int i = 0; i < count; i++)
             {
                 sum += number + sumadd;
                 sumadd += add;
+                if(sum>=int.MaxValue|| sum <= int.MinValue)
+                {
+                    throw new OverflowException();
+                }
             }
             return sum;
         }
